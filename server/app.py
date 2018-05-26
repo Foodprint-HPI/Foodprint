2a5af888fdbeddc5cba12af71a42934bfff39820
philipp.bode <philipp.bode@student.hpi.de>
2018-05-26 14:57:24 +0200

    Add Heroku config.

--- server/app.py
+++ server/app.py
@@ -2,9 +2,12 @@ from flask import (
     Flask,
     jsonify,
 )
+from flask.ext.heroku import Heroku
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+
+heroku = Heroku(app)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
@@ -12,7 +15,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 @app.route('/', methods=['GET'])
 @cross_origin()
 def play_command():
-    return jsonify(status=200)
+    return jsonify(status=200, db=app.config['SQLALCHEMY_DATABASE_URI'])
 
 
 if __name__ == '__main__':
