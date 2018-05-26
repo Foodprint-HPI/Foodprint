65cf7729bdeb560186396f6bda70685952a31cf9
Julian Weise <privat@julianweise.de>
2018-05-26 15:18:28 +0200

    merge requirements

---
+++ server/models.py
@@ -0,0 +1,13 @@
+from manage import db
+
+
+class Meal(db.Model):
+    __tablename__ = 'meal'
+
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True, unique=False)
+    recipe = db.Column(db.String(512), index=True, unique=False)
+    picture = db.Column(db.String(512), index=True, unique=False)
+
+    def __repr__(self):
+        return f'<Meal %r> {self.id}'
