e5fac701649396709e7de2eb51b3cd3291d1a513
philipp.bode <philipp.bode@student.hpi.de>
2018-05-26 14:19:34 +0200

    Move flask to directory; use honcho for multiple apps.

--- server/app.py
+++ server/app.py
@@ -2,11 +2,15 @@ from flask import (
     Flask,
     jsonify,
 )
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/', methods=['GET'])
+@cross_origin()
 def play_command():
     return jsonify(status=200)
 
