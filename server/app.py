4bc4ce815dcb92ca71ca4be3ca205b2ee8c6be28
philipp.bode <philipp.bode@student.hpi.de>
2018-05-26 15:02:15 +0200

    Use direct ext import.

--- server/app.py
+++ server/app.py
@@ -2,7 +2,7 @@ from flask import (
     Flask,
     jsonify,
 )
-from flask.ext.heroku import Heroku
+from flask_heroku import Heroku
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
