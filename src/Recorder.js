3e932b4248eff9e5753b2bdfa91737fa527db363
philipp.bode <philipp.bode@student.hpi.de>
2018-09-20 23:27:34 +0200

    Switch from deprecated navigator.getUserMedia to navigator.mediaDevices.getUserMedia to increase browser compatibility.

--- src/Recorder.js
+++ src/Recorder.js
@@ -40,37 +40,53 @@ class Recorder extends Component {
   }
 
   componentDidMount() {
-    // The getUserMedia interface is used for handling camera input.
-    // Some browsers need a prefix so here we're covering all the options
-    navigator.getMedia = ( navigator.getUserMedia ||
-      navigator.webkitGetUserMedia ||
-      navigator.mozGetUserMedia ||
-      navigator.msGetUserMedia);
-
-
-      if (!navigator.getMedia) {
-        //this.displayErrorMessage("Your browser doesn't have support for the navigator.getUserMedia interface.");
-      } else{
-
-        // Request the camera.
-        navigator.getMedia({
-          video: true
-        },
-
-        // Success callback
-        function(stream) {
-          this.refs.cameraStream.src = window.URL.createObjectURL(stream);
-
-          // Play the video element to start the stream.
-          this.refs.cameraStream.play();
-        }.bind(this),
-        // Error Callback
-        function(err){
-          //this.displayErrorMessage("There was an error with accessing the camera stream: " + err.name, err);
-        }.bind(this)
-      );
-
+    // Older browsers might not implement mediaDevices at all, so we set an empty object first
+    if (navigator.mediaDevices === undefined) {
+      navigator.mediaDevices = {};
     }
+
+    // Some browsers partially implement mediaDevices. We can't just assign an object
+    // with getUserMedia as it would overwrite existing properties.
+    // Here, we will just add the getUserMedia property if it's missing.
+    if (navigator.mediaDevices.getUserMedia === undefined) {
+      navigator.mediaDevices.getUserMedia = function(constraints) {
+
+        // First get ahold of the legacy getUserMedia, if present
+        var getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+        // Some browsers just don't implement it - return a rejected promise with an error
+        // to keep a consistent interface
+        if (!getUserMedia) {
+          return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+        }
+
+        // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+        return new Promise(function(resolve, reject) {
+          getUserMedia.call(navigator, constraints, resolve, reject);
+        });
+      }
+    } else {
+      console.log('navigator.mediaDevices.getUserMedia was straight found alright!')
+    }
+
+    navigator.mediaDevices.getUserMedia({ audio: false, video: true })
+    .then(function(stream) {
+      console.log('We got the stream!')
+      // Older browsers may not have srcObject
+      // if ("src" in this.refs.cameraStream) {
+      //   console.log('Source was not found.')
+        // this.refs.cameraStream.src = window.URL.createObjectURL(stream);
+        this.refs.cameraStream.srcObject = stream;
+      // } else {
+      //   console.log('src was already there.')
+      //   // Avoid using this in new browsers, as it is going away.
+      //   this.refs.cameraStream.src = window.URL.createObjectURL(stream);
+      // }
+    }.bind(this))
+    .catch(function(err) {
+      console.log('In tha error!')
+      console.log(err.name + ": " + err.message);
+    }.bind(this));
   }
 
   startCamera(event) {
@@ -233,7 +249,7 @@ class Recorder extends Component {
 
       <a href="#" id="start-camera" className="visible" onClick={this.startCamera} ref="startButton">Touch here to start the camera.</a>
       <div style={{"position": "relative", "overflow": "hidden", "width": "80vw", "height": "80vw", "margin":"auto"}}>
-      <video id="camera-stream" ref="cameraStream"></video>
+      <video id="camera-stream" ref="cameraStream" autoPlay="true"></video>
       <img ref="image" id="snap" />
       <div className="controls" ref="controls">
       <a href="#" id="delete-photo" title="Delete Photo" className="disabled" onClick={this.deletePhoto} ref="deletePhoto"><i className="far fa-trash-alt"></i></a>
