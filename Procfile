9ceff2c27751c51265c4cf727eeae1e7e23f6784
philipp.bode <philipp.bode@student.hpi.de>
2018-09-20 21:42:39 +0200

    Move node app to root for deploy.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: cd lowcarb-frontend && npm install && npm start
+web: npm install && npm start
--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export PYTHONPATH=$PYTHONPATH:$PWD
-
-gunicorn server.app:app -t 3000 --log-file=-
